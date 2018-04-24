27840a80c8aa5cc172c6a22deb71e3a81aaa5c19
TestSmirk <d.kc@qq.com>
2018-04-24 11:17:15 +0800

    router server and index

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var http = require("http");
-http.createServer(function (request,response){
-	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.end("Hello World");
-}).listen(8888);
-console.log("Server running at http://localhost:8888");
+var url = require("url");
+function start(route){
+	function onRequest(request,response){
+		var pathname = url.parse(request.url).pathname;
+		console.log("request for "+pathname+" received");
+		route(pathname);
+		response.writeHead(200,{"Content-Type":"text/plain"});
+		response.write("hello world");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+exports.start = start;
+
